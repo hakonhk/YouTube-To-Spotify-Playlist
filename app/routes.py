588de5ecb17fcbcfe55367e4bf7ac6d37ffdc46e
shrpboi <hakonhklausen@gmail.com>
2024-09-28 12:04:15 +0200

    Refactor ShazamAnalyzer to use AudioProcessor

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, render_template, request, jsonify
 from flask_socketio import emit
 from .utils.SpotifyPlaylistGenerator import SpotifyPlaylistGenerator
-from .utils.ShazamAnalyzer import ShazamAnalyzer
-from .utils.youtubeDownloader import youtubeDownloader
+from .utils.AudioProcessor import AudioProcessor
 import asyncio
 from tqdm import tqdm
 from app import socketio  # Import socketio from app
@@ -29,26 +28,22 @@ def analyze():
     
     return jsonify({"message": result, "estimated_time": estimated_time})
 
-async def shazam_analyze(audio_data):
+async def shazam_analyze(audio_processor):
     def progress_callback(progress, estimated_time, segment_processing_time):
         socketio.emit('progress_update', {'progress': progress, 'estimated_time': estimated_time, 'segment_processing_time': segment_processing_time})
 
-    analyzer = ShazamAnalyzer(audio_data, progress_callback=progress_callback)
-    results = await analyzer.run_analysis()
+    results = await audio_processor.run_analysis()
     return results
 
-def playlist_gen(shazam_results, playlist_name): 
+def playlist_gen(shazam_results, playlist_name):
     generator = SpotifyPlaylistGenerator(shazam_results, playlist_name)
     track_uris = generator.process_shazam_results()
     playlist_url = generator.create_spotify_playlist(track_uris)
     return f"Playlist created: {playlist_url}"
 
 async def main_analysis(youtube_link):
-    yt_dl = youtubeDownloader()
-    audio_data, video_title = yt_dl.download_audio(youtube_link)
-    
-    if audio_data is None:
-        raise FileNotFoundError("Downloaded audio data not found")
+    audio_processor = AudioProcessor(youtube_link)
+    await audio_processor.download_audio_to_bytes()
     
     # Initialize progress bar
     with tqdm(total=100, desc="Overall Progress") as pbar:
@@ -56,19 +51,19 @@ async def main_analysis(youtube_link):
         pbar.update(10)
         
         # Shazam analysis
-        shazam_results = await shazam_analyze(audio_data)
+        shazam_results = await shazam_analyze(audio_processor)
         pbar.update(70)
         
         # Playlist generation
-        result = playlist_gen(shazam_results, video_title)
+        result = playlist_gen(shazam_results, audio_processor.youtube_url)
         pbar.update(20)
     
     return result, "Estimated time not available"
 
 @socketio.on('connect')
 def handle_connect():
-    print('Client connected')
+    print("Client connected")
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    print('Client disconnected')
+    print("Client disconnected")
